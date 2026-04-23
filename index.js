a4ccbfbba29ce8ee9a45ab6d82ce406b487911e0
MavrodisMichael <m.mavrodis@gmail.com>
2026-04-23 11:30:45 +0200

    fix: add index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.json({
-    message: 'Hello DevOps !',
+    message: 'Hello DevOps ! DEV',
     status: 'ok',
     version: '1.0.0'
   })
